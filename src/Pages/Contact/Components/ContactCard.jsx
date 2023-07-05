c10e4dad3f8c7b9249ccc3e85e70af34795908a9
Aslan0N <thenzrv@gmail.com>
2023-07-05 10:39:50 +0400

    Dark mode

--- src/Pages/Contact/Components/ContactCard.jsx
+++ src/Pages/Contact/Components/ContactCard.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {MdLocationOn} from 'react-icons/md'
 import {MdPhoneInTalk} from 'react-icons/md'
 import {BsFillEnvelopeFill} from 'react-icons/bs'
+import Aos from 'aos'
 
 const ContactCard = () => {
+    useEffect(()=>{
+        Aos.init()
+    },[])
   return (
     <>
         <section id='contact-card'>
-        <h4>IF ANY QUERY</h4>
-        <h3>Contact Details</h3>
-            <div className="container">
+        <h4 data-aos="fade-up">IF ANY QUERY</h4>
+        <h3 data-aos="fade-up">Contact Details</h3>
+            <div className="container" data-aos="fade-up">
                 <div className="row">
                     <div className="col-12 col-sm-12 col-md-6 col-lg-4">
                         <div className="item">
--- src/Pages/Contact/Components/Form.jsx
+++ src/Pages/Contact/Components/Form.jsx
@@ -1,40 +1,44 @@
-import React from 'react'
+import Aos from 'aos'
+import React, { useEffect } from 'react'
 
 const Form = () => {
+    useEffect(()=>{
+        Aos.init()
+    },[])
   return (
     <>
         <section id='form'>
-            <h4>GET IN TOUCH</h4>
-            <h3>Leave A Message</h3>
+            <h4 data-aos="fade-up">GET IN TOUCH</h4>
+            <h3 data-aos="fade-up">Leave A Message</h3>
             <form>
                 <div className="row">
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6" data-aos="fade-up">
                         <div className="input">
                             <input type="text" placeholder='Firs Name*' />
                         </div>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6" data-aos="fade-up">
                         <div className="input">
                             <input type="text" placeholder='Last Name*' />
                         </div>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6" data-aos="fade-up">
                         <div className="input">
                             <input type="text" placeholder='Email*' />
                         </div>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-6 col-lg-6">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-6" data-aos="fade-up">
                         <div className="input">
                             <input type="text" placeholder='Subject' />
                         </div>
                     </div>
-                    <div className="col-12 col-sm-12 col-md-12 col-lg-12">
+                    <div className="col-12 col-sm-12 col-md-12 col-lg-12" data-aos="fade-up">
                         <div className="input bor">
                             <textarea name="" id="" cols="57" rows="5" className='textarea' placeholder='Message'></textarea>
                         </div>
                     </div>
                 </div>
-            <button>Send Message!</button>
+            <button data-aos="fade-up">Send Message!</button>
             </form>
         </section>
     </>
